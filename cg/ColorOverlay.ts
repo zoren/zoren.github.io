614db0bc97cd7fbb166ad05afd701ee313f88431
Søren Sjørup <zorren@gmail.com>
2016-05-20 14:40:54 +0200

    paint using canvas

--- cg/ColorOverlay.ts
+++ cg/ColorOverlay.ts
@@ -1,5 +1,4 @@
 /// <reference path="google.maps.d.ts" />
-
 function paintRectangles(map:google.maps.Map,
                 latinc:number, longinc:number,
                         initial:google.maps.LatLng,
@@ -8,8 +7,8 @@ function paintRectangles(map:google.maps.Map,
         for(var j = 0;j<longCount;j++){
         var bounds =
             new google.maps.LatLngBounds(
-                new google.maps.LatLng(initial.lat()+((i+1)*latinc), initial.lng()+(j*longinc)),
-                new google.maps.LatLng(initial.lat()+(i*latinc), initial.lng()+((j+1)*longinc)));
+                new google.maps.LatLng(initial.lat()+(i*latinc), initial.lng()+(j*longinc)),
+                new google.maps.LatLng(initial.lat()+((i+1)*latinc), initial.lng()+((j+1)*longinc)));
 
         var rectOpts : google.maps.RectangleOptions = {
             bounds: bounds,
@@ -21,8 +20,131 @@ function paintRectangles(map:google.maps.Map,
         var rectangle = new google.maps.Rectangle(rectOpts);
         rectangle.setMap(map);
         }
-    }        
-}    
+    }
+}
+
+function paintRectangle(map:google.maps.Map,
+                latinc:number, longinc:number,
+                        initial:google.maps.LatLng,
+                        latCount:number, longCount:number){
+        var bounds =
+            new google.maps.LatLngBounds(
+                initial,
+                new google.maps.LatLng(initial.lat()+(latCount*latinc), initial.lng()+(longCount*longinc)));
+
+    var rectOpts : google.maps.RectangleOptions = {
+        bounds: bounds,
+        fillColor: 'red',
+        strokeWeight : 0
+    //  editable: true
+    };            
+    // Define a rectangle and set its editable property to true.
+    var rectangle = new google.maps.Rectangle(rectOpts);
+    rectangle.setMap(map);
+}
+
+function getNorthWest(bounds:google.maps.LatLngBounds) {
+    return new google.maps.LatLng(bounds.getNorthEast().lat(), bounds.getSouthWest().lng());
+}
+
+function getSouthEast(bounds:google.maps.LatLngBounds) {
+    return new google.maps.LatLng(bounds.getSouthWest().lat(), bounds.getNorthEast().lng());
+}
+
+class ColorOverlayView extends google.maps.OverlayView {
+    private div : HTMLDivElement = null
+    private canvas : HTMLCanvasElement = null
+    private ctx : CanvasRenderingContext2D = null
+    constructor(private bounds:google.maps.LatLngBounds,
+                private map:google.maps.Map){
+        super()
+        this.setMap(map)
+    }
+    onAdd(){
+        var div = document.createElement('div');
+        div.style.borderStyle = 'none';
+        div.style.borderWidth = '0px';
+        div.style.position = 'absolute';
+        
+        var canvas = document.createElement('canvas');
+        canvas.style.width = '100%'
+        canvas.style.height = '100%'
+        
+        var ctx = canvas.getContext("2d");
+//         ctx.fillStyle = 'black'
+//         ctx.fillRect(0, 0, 1, 1)
+//         ctx.fillRect(1, 1, 1, 1)
+// //        ctx.f
+         div.appendChild(canvas); 
+        
+        this.div = div;
+        this.canvas = canvas;
+        this.ctx = ctx;
+        var panes = this.getPanes();
+        panes.overlayLayer.appendChild(div);        
+    }
+    draw(){
+        var overlayProjection = this.getProjection();
+
+        // Retrieve the south-west and north-east coordinates of this overlay
+        // in LatLngs and convert them to pixel coordinates.
+        // We'll use these coordinates to resize the div.
+        var sw = overlayProjection.fromLatLngToDivPixel(this.bounds.getSouthWest());
+        var ne = overlayProjection.fromLatLngToDivPixel(this.bounds.getNorthEast());
+
+        let w = ne.x - sw.x
+        let h = sw.y - ne.y
+        let canvas = this.canvas
+        console.log(w)
+        console.log(h)
+        canvas.width = w
+        canvas.height = h
+        let ctx = this.ctx
+        ctx.fillStyle = 'black'
+
+        const latinc = 1/1000;
+        const longinc = 1/500;
+        const latCount = 15;
+        const longCount = 10;
+        let initial = this.bounds.getSouthWest()
+
+        let initPoint = overlayProjection.fromLatLngToDivPixel(getNorthWest(this.bounds))
+        
+        for(var i = 0;i<latCount;i++){
+            for(var j = 0;j<longCount;j++){
+                var bounds =
+                    new google.maps.LatLngBounds(
+                        new google.maps.LatLng(initial.lat()+i*latinc, initial.lng()+j*longinc),
+                        new google.maps.LatLng(initial.lat()+((i+1)*latinc), initial.lng()+((j+1)*longinc)));
+                let nw = overlayProjection.fromLatLngToDivPixel(getNorthWest(bounds))
+                let se = overlayProjection.fromLatLngToDivPixel(getSouthEast(bounds))
+                ctx.globalAlpha = 0.5                        
+                ctx.fillStyle = '#'+Math.floor(Math.random()*16777215).toString(16)
+                ctx.fillRect(nw.x-initPoint.x, nw.y - initPoint.y, se.x - nw.x, se.y - nw.y)
+            }
+        }
+
+        // Resize the image's div to fit the indicated dimensions.
+        var div = this.div;
+        div.style.left = sw.x + 'px';
+        div.style.top = ne.y + 'px';                      
+        div.style.width = w + 'px';
+        div.style.height = h + 'px';
+    }
+    remove(){
+        this.div.parentNode.removeChild(this.div);
+        this.div = null;        
+    }
+}
+
+function paintRectanglesCanvas(map:google.maps.Map,
+                                latinc:number, longinc:number,
+                                initial:google.maps.LatLng,
+                                latCount:number, longCount:number) {                                    
+    let ne = new google.maps.LatLng(initial.lat() + latinc * latCount, initial.lng() + longinc * longCount)
+    let bounds = new google.maps.LatLngBounds(initial, ne)
+    new ColorOverlayView(bounds, map)
+}
 
 function initMap(center:google.maps.LatLng) {
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -32,16 +154,16 @@ function initMap(center:google.maps.LatLng) {
     });
     const latinc = 1/1000;
     const longinc = 1/500;
-
-    const initialLat = 55.70;
-    const initialLong = 12.54;
-    const initial = new google.maps.LatLng( 55.69, 12.54);
-    console.log(initial);
-
     const latCount = 15;
     const longCount = 10;
 
-    paintRectangles(map, latinc, longinc,
+    const initial = new google.maps.LatLng( 55.69, 12.54);
+    console.log(initial);
+
+
+    //paintRectangle
+    paintRectanglesCanvas
+    (map, latinc, longinc,
                             initial,
                             latCount, longCount);
 }
--- cg/js/tsc.js
+++ cg/js/tsc.js
@@ -1,7 +1,12 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 function paintRectangles(map, latinc, longinc, initial, latCount, longCount) {
     for (var i = 0; i < latCount; i++) {
         for (var j = 0; j < longCount; j++) {
-            var bounds = new google.maps.LatLngBounds(new google.maps.LatLng(initial.lat() + ((i + 1) * latinc), initial.lng() + (j * longinc)), new google.maps.LatLng(initial.lat() + (i * latinc), initial.lng() + ((j + 1) * longinc)));
+            var bounds = new google.maps.LatLngBounds(new google.maps.LatLng(initial.lat() + (i * latinc), initial.lng() + (j * longinc)), new google.maps.LatLng(initial.lat() + ((i + 1) * latinc), initial.lng() + ((j + 1) * longinc)));
             var rectOpts = {
                 bounds: bounds,
                 fillColor: '#' + Math.floor(Math.random() * 16777215).toString(16),
@@ -12,6 +17,95 @@ function paintRectangles(map, latinc, longinc, initial, latCount, longCount) {
         }
     }
 }
+function paintRectangle(map, latinc, longinc, initial, latCount, longCount) {
+    var bounds = new google.maps.LatLngBounds(initial, new google.maps.LatLng(initial.lat() + (latCount * latinc), initial.lng() + (longCount * longinc)));
+    var rectOpts = {
+        bounds: bounds,
+        fillColor: 'red',
+        strokeWeight: 0
+    };
+    var rectangle = new google.maps.Rectangle(rectOpts);
+    rectangle.setMap(map);
+}
+function getNorthWest(bounds) {
+    return new google.maps.LatLng(bounds.getNorthEast().lat(), bounds.getSouthWest().lng());
+}
+function getSouthEast(bounds) {
+    return new google.maps.LatLng(bounds.getSouthWest().lat(), bounds.getNorthEast().lng());
+}
+var ColorOverlayView = (function (_super) {
+    __extends(ColorOverlayView, _super);
+    function ColorOverlayView(bounds, map) {
+        _super.call(this);
+        this.bounds = bounds;
+        this.map = map;
+        this.div = null;
+        this.canvas = null;
+        this.ctx = null;
+        this.setMap(map);
+    }
+    ColorOverlayView.prototype.onAdd = function () {
+        var div = document.createElement('div');
+        div.style.borderStyle = 'none';
+        div.style.borderWidth = '0px';
+        div.style.position = 'absolute';
+        var canvas = document.createElement('canvas');
+        canvas.style.width = '100%';
+        canvas.style.height = '100%';
+        var ctx = canvas.getContext("2d");
+        div.appendChild(canvas);
+        this.div = div;
+        this.canvas = canvas;
+        this.ctx = ctx;
+        var panes = this.getPanes();
+        panes.overlayLayer.appendChild(div);
+    };
+    ColorOverlayView.prototype.draw = function () {
+        var overlayProjection = this.getProjection();
+        var sw = overlayProjection.fromLatLngToDivPixel(this.bounds.getSouthWest());
+        var ne = overlayProjection.fromLatLngToDivPixel(this.bounds.getNorthEast());
+        var w = ne.x - sw.x;
+        var h = sw.y - ne.y;
+        var canvas = this.canvas;
+        console.log(w);
+        console.log(h);
+        canvas.width = w;
+        canvas.height = h;
+        var ctx = this.ctx;
+        ctx.fillStyle = 'black';
+        var latinc = 1 / 1000;
+        var longinc = 1 / 500;
+        var latCount = 15;
+        var longCount = 10;
+        var initial = this.bounds.getSouthWest();
+        var initPoint = overlayProjection.fromLatLngToDivPixel(getNorthWest(this.bounds));
+        for (var i = 0; i < latCount; i++) {
+            for (var j = 0; j < longCount; j++) {
+                var bounds = new google.maps.LatLngBounds(new google.maps.LatLng(initial.lat() + i * latinc, initial.lng() + j * longinc), new google.maps.LatLng(initial.lat() + ((i + 1) * latinc), initial.lng() + ((j + 1) * longinc)));
+                var nw = overlayProjection.fromLatLngToDivPixel(getNorthWest(bounds));
+                var se = overlayProjection.fromLatLngToDivPixel(getSouthEast(bounds));
+                ctx.globalAlpha = 0.5;
+                ctx.fillStyle = '#' + Math.floor(Math.random() * 16777215).toString(16);
+                ctx.fillRect(nw.x - initPoint.x, nw.y - initPoint.y, se.x - nw.x, se.y - nw.y);
+            }
+        }
+        var div = this.div;
+        div.style.left = sw.x + 'px';
+        div.style.top = ne.y + 'px';
+        div.style.width = w + 'px';
+        div.style.height = h + 'px';
+    };
+    ColorOverlayView.prototype.remove = function () {
+        this.div.parentNode.removeChild(this.div);
+        this.div = null;
+    };
+    return ColorOverlayView;
+}(google.maps.OverlayView));
+function paintRectanglesCanvas(map, latinc, longinc, initial, latCount, longCount) {
+    var ne = new google.maps.LatLng(initial.lat() + latinc * latCount, initial.lng() + longinc * longCount);
+    var bounds = new google.maps.LatLngBounds(initial, ne);
+    new ColorOverlayView(bounds, map);
+}
 function initMap(center) {
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 16,
@@ -20,12 +114,10 @@ function initMap(center) {
     });
     var latinc = 1 / 1000;
     var longinc = 1 / 500;
-    var initialLat = 55.70;
-    var initialLong = 12.54;
-    var initial = new google.maps.LatLng(55.69, 12.54);
-    console.log(initial);
     var latCount = 15;
     var longCount = 10;
-    paintRectangles(map, latinc, longinc, initial, latCount, longCount);
+    var initial = new google.maps.LatLng(55.69, 12.54);
+    console.log(initial);
+    paintRectanglesCanvas(map, latinc, longinc, initial, latCount, longCount);
 }
 //# sourceMappingURL=tsc.js.map
